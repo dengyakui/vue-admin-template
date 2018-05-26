4e3795077f4b94b0d69c1967a8083357abc84f22
dengyakui <297354229@qq.com>
2018-05-26 22:14:43 +0800

    update

---
+++ src/views/helloworld/slider.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <el-slider
+    :min="10"
+    :max="80"
+    :show-stops="true"
+    vertical
+    @change="handleChange"
+    range="true"
+    height="200px"
+    :step="10">
+
+  </el-slider>
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    handleChange(val) {
+      console.log(val)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
